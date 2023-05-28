b680f939ffead7b481380f9f0a546e0b98ab7584
ichetiva <feoktistovvladislav@outlook.com>
2023-05-28 18:55:33 +0300

    feat: add anime model

---
+++ backend/src/models/anime.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String
+
+from ._mixins import TimestampMixin
+from .base import Base
+
+
+class Anime(Base, TimestampMixin):
+    __tablename__ = "anime"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(512))
+    type_ = Column(String(32))
+    publishing_year = Column(Integer)
+
+    def __repr__(self) -> str:
+        return self.title
