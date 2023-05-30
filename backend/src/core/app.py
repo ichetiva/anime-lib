eabc4d1060cc8f522d4d46d1ac8ec5eb161e0827
ichetiva <feoktistovvladislav@outlook.com>
2023-05-30 17:14:08 +0300

    feat: add cors middleware

--- backend/src/core/app.py
+++ backend/src/core/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api import router as api_router
 
@@ -7,5 +8,13 @@ def create_app() -> FastAPI:
     app_ = FastAPI()
 
     app_.include_router(api_router)
+
+    app_.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     return app_
